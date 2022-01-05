ebd33e0cef26c3846752f30d37dc70a8eaba1583
Atsushi Furukawa <atsushifx@aglabo.com>
2022-01-05 11:11:09 +0900

    課題3:循環小数についてプログラミング

--- edx/TiTechX-CS101x-CSwithProgramming/kadai3/max.rb
+++ edx/TiTechX-CS101x-CSwithProgramming/kadai3/max.rb
@@ -1,15 +1,12 @@
 # max.rb
-# input: numbet list
-# output: max num
+# ����: �����̗�
+# �o��: �ő�l
 
 a = gets().split.map(&:to_i)
 n = a.length
 max = -100000
 maxj = -1
 for j in 0..(n-1)
-	if max < a[j]
-		max = a[j]
-		maxj = j
-	end
+
 end
 puts(max, maxj)
