e5b60d933e3305db76403e24febcd063b5490964
Atsushi Furukawa <atsushifx@aglabo.com>
2022-02-20 04:44:39 +0900

    cousera: java prog - examination: gladlib   codon counter exam

---
+++ coursera/duke-javaProgData/week02-randomstory/exam-GladLibs/CodonCounter.java
@@ -0,0 +1,118 @@
+import java.util.*;
+import edu.duke.*;
+
+/**
+ * CodonCounter
+ *   create codon map anf count codons
+ * 
+ * @author  Furukawa Atsushi <atsushifx@gmail.com>
+ * @version 1.0.0
+ */
+public class CodonCounter
+{
+    private HashMap<String, Integer> myCodonMap;
+    
+    /**
+     * constructor
+     */
+    public CodonCounter() {
+        myCodonMap = new HashMap<String, Integer>();
+    }
+    
+    /**
+     * buildCodonMap
+     *   build codon map from DNA strand
+     *   
+     */
+    void buildCodonMap(int start, String dna) {
+        myCodonMap.clear();
+        dna = dna.trim();
+        int len = dna.length();
+        for (int i = start; i<len; i+=3) {
+            if (i+3 <= len) {
+                String codon=dna.substring(i, i+3);
+                addCodon(codon);
+            }
+        }
+    }
+    
+    /**
+     * addCodon
+     *   add codon to codon map
+     */
+    private void addCodon(String codon) {
+        if (codon.length() != 3){ return; }
+        
+        if (myCodonMap.containsKey(codon)) {
+            int cnt = myCodonMap.get(codon);
+            myCodonMap.put(codon, cnt + 1);
+        } else {
+            myCodonMap.put(codon, 1);
+        }
+    }
+    
+    /**
+     * getMostCommonCodon 
+     */
+    String getMostCommonCodon() {
+        String commonCodon = "";
+        int maxCnt = 0;
+        
+        for (String codon : myCodonMap.keySet()) {
+            int cnt = myCodonMap.get(codon);
+            
+            if (cnt>maxCnt || maxCnt==0) {
+                maxCnt = cnt;
+                commonCodon = codon;
+            }
+        }
+        return commonCodon;
+    }
+    
+    /**
+     * mostCommomnCodonList
+     */
+    String mostCommonCodonList(String dna) {
+        String list = "";
+        for (int st = 0; st<=2; st++) {
+            buildCodonMap(st, dna);
+            String codon = getMostCommonCodon();
+            int cnt = myCodonMap.get(codon);
+            list += st +". " + codon + "  " + cnt + "\n";
+        }
+        return list;
+    }
+        
+    /**
+     *  printCodonCounts
+     */
+    void printCodonCounts(int start, int end) {
+        for (String codon : myCodonMap.keySet()) {
+            int cnt = myCodonMap.get(codon);
+            if (start<=cnt && (cnt<=end || end<=0)) {
+                System.out.println(codon + "\t" + cnt);
+            }
+        }
+    }
+    
+    
+    
+    /**
+     * tester
+     *   test build codon map
+     */
+    public void tester() {
+        System.out.println("\n 　test codon map");
+        FileResource fr = new FileResource("data/smalldna.txt");
+        String dna = fr.asString();
+        
+        for (int st=0; st<=2; st++) {
+            System.out.println("\n  start " + st);
+            buildCodonMap(st, dna);
+            String codon = getMostCommonCodon();
+            int cnt = myCodonMap.get(codon);
+            System.out.println("MostCommon: " + codon + " " + cnt);
+            printCodonCounts(0, 15);
+        } 
+    }
+}
