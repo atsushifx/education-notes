0b6e11e29860c2fc9043755da31ec97e1ec5aef3
Atsushi Furukawa <atsushifx@aglabo.com>
2022-03-11 23:08:23 +0900

    cousera: java   examination: searching quakes   練習問題用にメソッド内部を書き換え

--- coursera/duke-java4-priciple/week01-earthquakes/exercise1-searching/LargestQuakes.java
+++ coursera/duke-java4-priciple/week01-earthquakes/exercise1-searching/LargestQuakes.java
@@ -54,8 +54,8 @@ public class LargestQuakes
     public void findLargestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
         // String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-        // String source = "data/nov20quakedata.atom";
-        String source = "data/nov20quakedatasmall.atom";  // for Assignment5
+        String source = "data/nov20quakedata.atom";  // for examination
+        // String source = "data/nov20quakedatasmall.atom";  // for Assignment5
         
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("\n  read data for "+list.size());
