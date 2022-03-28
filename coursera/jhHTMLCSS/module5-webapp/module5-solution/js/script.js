83cfb727bac90db2dd905a8a6fb3f81ab7e24c5f
Atsushi Furukawa <atsushifx@aglabo.com>
2022-03-29 01:11:55 +0900

    cousera: html/css/js   examination 5   Specialをランダムに選ぶように変更

--- coursera/jhHTMLCSS/module5-webapp/module5-solution/js/script.js
+++ coursera/jhHTMLCSS/module5-webapp/module5-solution/js/script.js
@@ -100,7 +100,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
         // var chosenCategoryShortName = ....
-        let chooseRandomCategoryShortName = "SP";
+        let chooseRandomCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
