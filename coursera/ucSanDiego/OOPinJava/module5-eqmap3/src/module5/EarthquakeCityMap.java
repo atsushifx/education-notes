5e82d04b2e285080796b7f572db854124b674cb9
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2022-06-13 01:02:56 +0900

    fix: read earthquakes from web

--- coursera/ucSanDiego/OOPinJava/module5-eqmap3/src/module5/EarthquakeCityMap.java
+++ coursera/ucSanDiego/OOPinJava/module5-eqmap3/src/module5/EarthquakeCityMap.java
@@ -67,7 +67,7 @@ public class EarthquakeCityMap extends PApplet {
 		size(900, 700, OPENGL);
 		if (offline) {
 		    map = new UnfoldingMap(this, 200, 50, 650, 600, new MBTilesMapProvider(mbTilesString));
-		    earthquakesURL = "2.5_week.atom";  // The same feed, but saved August 7, 2015
+		    // earthquakesURL = "2.5_week.atom";  // The same feed, but saved August 7, 2015
 		}
 		else {
 			map = new UnfoldingMap(this, 200, 50, 650, 600, new OpenStreetMap.OpenStreetMapProvider());
