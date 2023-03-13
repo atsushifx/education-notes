09e63bd2d366e98b2a667e8a5f85f6bf9538bd02
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2023-03-14 00:59:25 +0900

    add: racket : Hello Worldを追加

---
+++ self-educations/languages/scheme/Scheme-SuperBeginner/01-helloworld/helllo.rkt
@@ -0,0 +1,8 @@
+#lang racket
+;; Copyright (c) 2023 Furukawa, Atsushi <atsushifx@aglabo.com>
+;;
+;; This software is released under the MIT License.
+;; https://opensource.org/licenses/MIT
+
+(display  "Hello 世界\n")
+"Hello, World"
