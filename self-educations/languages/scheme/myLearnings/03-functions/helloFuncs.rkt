10f3e38b3417fb3eab29e51f69c4e3661af962ac
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2023-03-20 08:52:55 +0900

    add: lisp/myLrarnings: hellofuncsを追加

---
+++ self-educations/languages/scheme/myLearnings/03-functions/helloFuncs.rkt
@@ -0,0 +1,10 @@
+;; Copyright (c) 2023 Furukawa, Atsushi <atsushifx@aglabo.com>
+;;
+;; This software is released under the MIT License.
+;; https://opensource.org/licenses/MIT
+#lang racket
+
+(define (fHello x)  (display (string-append "おはよう " x "!!\n")) )
+(fHello "世界")
+
+
